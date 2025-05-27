34d8148772c8e5e8516111317389d7714c14a3bc
Hatixhe97 <hatixhe.idar@gmail.com>
2025-05-28 00:21:23 +0200

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,64 @@
+
+import styles from './Footer.module.css'
+import facebook from '../../assets/icons/facebook.svg'
+import insta from '../../assets/icons/insta.svg'
+import twitter from '../../assets/icons/twitter.svg'
+
+const Footer =()=>{
+    return(
+        <div className={styles.footercontainer}>
+        <div className={styles.footer}>
+            <div>
+                <p>GREENMIND</p>
+                <p className={styles.p2}>We help you find your dream plant</p>
+                <div className={styles.icons}>
+                    <img src={facebook} alt="facebook" />
+                    <img src={insta} alt="instagram" />
+                    <img src={twitter} alt="twitter" />
+                </div>
+
+            </div>
+
+            <div className={styles.select}>
+                <div>
+                    <p className={styles.p}>Information</p>
+                    <div className={styles.p1}>
+                     <p>About</p>
+                     <p>Product</p>
+                     <p>Blog</p>
+                    </div>
+                    
+                </div>
+
+                <div>
+                    <p className={styles.p}>Company</p>
+                    <div className={styles.p1}>
+                      <p>Community</p>
+                      <p>Career</p>
+                      <p>Our story</p>
+                    </div>
+
+                </div>
+
+                <div >
+                    <p className={styles.p}>Contact</p>
+                    <div className={styles.p1}>
+                     <p>Getting Started</p>
+                     <p>Pricing</p>
+                     <p>Resources</p>
+                    </div>
+                    
+                </div>
+
+            </div>
+
+            
+        </div>
+
+         <p className={styles.p3}>2023 all Right Reserved Term of use GREENMIND</p>
+        </div>
+    )
+}
+
+
+export default Footer;
