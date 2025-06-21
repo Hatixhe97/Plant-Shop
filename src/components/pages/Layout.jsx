80fd0b468a60399aee7a77aad135ed975a04e4f8
Hatixhe97 <hatixhe.idar@gmail.com>
2025-06-22 00:37:38 +0200

    change name

--- src/components/pages/Layout.jsx
+++ src/components/pages/Layout.jsx
@@ -1,5 +1,5 @@
-import Footer from "../Footer/Footer"
-import Header from "../header/header"
+import Footer from "../Footer/Footer";
+import Header from "../header/header";
 import { Outlet } from 'react-router-dom';
 
 const Layout =()=>{
